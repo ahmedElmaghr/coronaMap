dbf6822e5a55361b1482db12fba5613f576c7e7c
unknown <elmaghraoui.ahmed@gmail.com>
2020-03-22 02:43:29 +0100

    landscape lock screen for mobile

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
+window.screen.orientation.lock('landscape');
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
