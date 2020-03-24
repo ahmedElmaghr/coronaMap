0add15e526508136665de4fd4cf47d12859f91aa
ELMAGHRAOUI <ahmed.2.elmaghraoui.external@atos.net>
2020-03-24 09:16:43 +0100

    corriger l'erreur : Impossible d’obtenir la propriété  « lock » d’une référence null ou non définie

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-window.screen.orientation.lock('landscape');
+// window.screen.orientation.lock('landscape');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
