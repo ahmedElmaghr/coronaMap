730e2f57e72da71f6ae7bd7d2ecea8aec7fe8dbc
unknown <elmaghraoui.ahmed@gmail.com>
2020-04-10 18:37:48 +0200

    update panel information

--- src/Components/panelchart/Panel.js
+++ src/Components/panelchart/Panel.js
@@ -1,144 +1,147 @@
 import React from "react";
 import "./Panel.css";
-import  StringUtils  from "../../Utils/StringUtils";
+import StringUtils from "../../Utils/StringUtils";
 
 export default class Panel extends React.Component {
-  
-  componentWillMount(){
-    document.addEventListener("click",this.handleClick,false)
+  componentWillMount() {
+    document.addEventListener("click", this.handleClick, false);
   }
 
-  componentWillUnmount(){
-    document.removeEventListener("click",this.handleClick,false)
+  componentWillUnmount() {
+    document.removeEventListener("click", this.handleClick, false);
   }
 
-  handleClick=(e)=>{
-    if(this.node.contains(e.target)){
+  handleClick = (e) => {
+    if (this.node.contains(e.target)) {
       return;
     }
-    this.handleClickOutside(e)
-  }
+    this.handleClickOutside(e);
+  };
 
-  handleClickOutside=(e)=>{
-    if(e.target.tagName!=="path" && e.target.tagName!=="circle"){
+  handleClickOutside = (e) => {
+    if (e.target.tagName !== "path" && e.target.tagName !== "circle") {
       this.props.closePanel();
     }
-  }
+  };
 
   render() {
-    let { stat, opacity,zIndex, x, y } = this.props;
+    let { stat, opacity, zIndex, x, y } = this.props;
     return (
-      <div ref={node => this.node=node}>
-        <div id="panelStat"
+      <div ref={(node) => (this.node = node)}>
+        <div
+          id="panelStat"
           className="mapboxgl-popup mapboxgl-popup-anchor-bottom"
-          style={{ top: y + 'px', left: x + 'px', opacity: opacity ,zIndex: zIndex}}
+          style={{
+            top: y + "px",
+            left: x + "px",
+            opacity: opacity,
+            zIndex: zIndex,
+          }}
         >
           <div className="mapboxgl-popup-tip"></div>
           <div className="mapboxgl-popup-content">
-            <div
-              className="maphub-popup-inner maphub-popup-text ps"
-              style={{ maxHeight: 411 + "px", maxWidth: 840 + "px" }}
-            >
-              <span className="closePanel" onClick={()=>{this.props.closePanel()}}>X</span>
-              <div className="nicetext">  
+            <div>
+              <div className="nicetext">
                 <div>
                   <div className="nicetext-title">
-                      <span>{stat ? stat.Country : ""}</span>
+                    <span>{stat ? stat.Country : ""}</span>
                   </div>
                 </div>
                 <div>
-                  <p
-                    style={{
-                      color: "green",
-                      // borderTop: "solid 1px",
-                      margin: "2px 2px 2px 2px",
-                      // borderRadius: 6 + "px",
-                      fontSize: 12+'px',
-                      fontWeight: 'bold'
-                    }}
-                  >
-                    Recovered :{" "}
+                  <div>
+                    <table class="table table-bordered  ">
+                      <thead>
+                        <tr className="table-primary" >
+                          <th colSpan="2" className ="rowHead" >Today's update</th>
+                        </tr>
+                      </thead>{" "}
+                      <tbody className="tbody">
+                        <tr className="newCases">
+                          <td>New cases</td>
+                          <td className="center">
+                            {stat && StringUtils.isNotEmpty(stat.NewCases)
+                              ? stat.NewCases
+                              : "0"}
+                          </td>
+                        </tr>
+                        <tr className="newDeaths">
+                          <td>New deaths</td>
+                          <td className="center">
+                            {stat && StringUtils.isNotEmpty(stat.NewDeaths)
+                              ? stat.NewDeaths
+                              : "0"}
+                          </td>
+                        </tr>
+                      </tbody>
+                      <thead>
+                        <tr className="table-info" >
+                          <th colSpan="2" className="rowHead">Total update</th>
+                        </tr>
+                      </thead>{" "}
+                      <tbody>
+                      <tr className="totalRecovered">
+                          <td>Total recovered</td>
+                          <td className="center">
+                            {stat && StringUtils.isNotEmpty(stat.TotalRecovered)
+                              ? stat.TotalRecovered
+                              : "0"}
+                          </td>
+                        </tr>
+                        <tr className="totalDeaths">
+                          <td>Total deaths</td>
+                          <td className="center">
+                            {stat && StringUtils.isNotEmpty(stat.TotalDeaths)
+                              ? stat.TotalDeaths
+                              : "0"}
+                          </td>
+                        </tr>
+                        <tr className="totalCases">
+                          <td>Total cases</td>
+                          <td>
+                            {stat && StringUtils.isNotEmpty(stat.TotalCases)
+                              ? stat.TotalCases
+                              : "0"}
+                          </td>
+                        </tr>  
+                      </tbody>
+                    </table>
+                  </div>
+                  {/* <p className="newCases">
+                    New cases:{" "}
+                    {stat && StringUtils.isNotEmpty(stat.NewCases)
+                      ? stat.NewCases
+                      : "0"}
+                  </p>
+                  <p className="newDeaths">
+                    New deaths :{" "}
+                    {stat && StringUtils.isNotEmpty(stat.NewDeaths)
+                      ? stat.NewDeaths
+                      : "0"}
+                  </p>
+                  <p className="totalRecovered">
+                    Total recovered :{" "}
                     {stat && stat.TotalRecovered ? stat.TotalRecovered : "0"}{" "}
                   </p>
-                  <p
-                    style={{
-                      color: "darkred",
-                      // borderTop: "solid 1px",
-                      margin: "2px 2px 2px 2px",
-                      // borderRadius: 6 + "px"
-                      fontSize: 12+'px',
-                      fontWeight: 'bold'
-                    }}
-                  >
-                    New cases:{" "}
-                    {(stat && StringUtils.isNotEmpty(stat.NewCases)) ? stat.NewCases : "0"}
-
+                  <p className="totalDeaths">
+                    Total deaths :{" "}
+                    {stat && StringUtils.isNotEmpty(stat.TotalDeaths)
+                      ? stat.TotalDeaths
+                      : "0"}
                   </p>
-                  <p
-                    style={{
-                      color: "black",
-                      // borderTop: "solid 1px",
-                      margin: "2px 2px 2px 2px",
-                      // borderRadius: 6 + "px"
-                      fontSize: 12+'px',
-                      fontWeight: 'bold'
-                    }}
-                  >
-                    New deaths :{" "}
-                    {(stat && StringUtils.isNotEmpty(stat.NewDeaths)) ? stat.NewDeaths : "0"}
-                  </p>
-                  <p
-                    style={{
-                      color: "black",
-                      // borderTop: "solid 1px",
-                      margin: "2px 2px 2px 2px",
-                      // borderRadius: 6 + "px"
-                      fontSize: 12+'px',
-                      fontWeight: 'bold'
-                    }}
-                  >
-                    Total deaths : {(stat && StringUtils.isNotEmpty(stat.TotalDeaths)) ? stat.TotalDeaths : "0"}
-                  </p>
-                  <p
-                    style={{
-                      color: "blue",
-                      // borderTop: "solid 1px",
-                      margin: "2px 2px 2px 2px",
-                      // borderRadius: 6 + "px"
-                      fontSize: 12+'px',
-                      fontWeight: 'bold'
-                    }}
-                  >
+                  <p className="totalCases">
                     Total cases :{" "}
                     {stat && stat.TotalCases ? stat.TotalCases : "0"}
-                  </p>
+                  </p> */}
                 </div>
-              </div>
-              <div className="ps__rail-x" style={{ left: 0 + "px", top: 0 + "px" }}>
-                <div
-                  className="ps__thumb-x"
-                  tabIndex="0"
-                  style={{ left: 0 + "px", width: 0 + "px" }}
-                ></div>
-              </div>
-              <div
-                className="ps__rail-y"
-                style={{ top: 0 + "px", right: 0 + "px" }}
-              >
-                <div
-                  className="ps__thumb-y"
-                  tabIndex="0"
-                  style={{ top: 0 + "px", height: 0 + "px" }}
-                ></div>
               </div>
             </div>
           </div>
         </div>
-      </div >
+      </div>
     );
   }
 
-  getData = data => {
+  getData = (data) => {
     if (data) {
       return [data.TotalCases, data.TotalDeaths, data.TotalRecovered];
     } else {
