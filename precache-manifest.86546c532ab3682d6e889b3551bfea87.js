ef1e7f63fa1efc5340bc0817ddaab392e2d86b34
Ahmed <elmaghraoui.ahmed@gmail.com>
2021-12-26 16:29:05 +0100

    Updates

---
+++ precache-manifest.86546c532ab3682d6e889b3551bfea87.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ae888c0558967f83f02752243a6998e",
+    "url": "/covid19MapRegion/index.html"
+  },
+  {
+    "revision": "ef650fdf630a93d29607",
+    "url": "/covid19MapRegion/static/css/2.06afed8f.chunk.css"
+  },
+  {
+    "revision": "811b78709548aa3589f6",
+    "url": "/covid19MapRegion/static/css/main.7f7921b7.chunk.css"
+  },
+  {
+    "revision": "ef650fdf630a93d29607",
+    "url": "/covid19MapRegion/static/js/2.a3b2f6ec.chunk.js"
+  },
+  {
+    "revision": "86493497955f2c03ad4534f44ae20baa",
+    "url": "/covid19MapRegion/static/js/2.a3b2f6ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "811b78709548aa3589f6",
+    "url": "/covid19MapRegion/static/js/main.dd029c48.chunk.js"
+  },
+  {
+    "revision": "67c41500f2f9d56ffa51",
+    "url": "/covid19MapRegion/static/js/runtime-main.51bb4fb3.js"
+  },
+  {
+    "revision": "2df50a8c6e605a30370a98fee967de8b",
+    "url": "/covid19MapRegion/static/media/countries.2df50a8c.tsv"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/covid19MapRegion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/covid19MapRegion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/covid19MapRegion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/covid19MapRegion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/covid19MapRegion/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "f521b2f80b2086dc041767c8761dd0c1",
+    "url": "/covid19MapRegion/static/media/results_coronavirus.f521b2f8.csv"
+  }
+]);
